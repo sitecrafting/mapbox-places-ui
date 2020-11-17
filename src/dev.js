ef4810f9ae741406a91e14e379597a39863ecbae
Coby Tamayo <ctamayo@sitecrafting.com>
2020-11-17 11:32:29 -0800

    make the dev environment a little more flexible

--- src/dev.js
+++ src/dev.js
@@ -4,14 +4,19 @@ import MapboxPlaces from './MapboxPlaces'
 
 const placesUi = document.getElementById('mapbox-places-ui')
 
+// Assume the entirety of the query string is the Mapbox access token,
+// (minus the "?").
+const token = location.search.slice(1)
+
 ReactDOM.render(
   <MapboxPlaces
-    mapboxToken=""
+    mapboxToken={token}
     initialValue={placesUi.dataset.value}
     textInputProps={{
       name: "coordinates"
     }}
     coordinatesInputProps={{
+      // On a real project, you'll probably want this to be hidden.
       type: "text",
       name: "coordinates",
     }}
