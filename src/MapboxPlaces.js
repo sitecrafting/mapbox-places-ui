1cb160c6158c25ff4caa6b01a23338be8035a03e
Coby Tamayo <ctamayo@sitecrafting.com>
2021-03-03 16:17:16 -0800

    Support modifying Place name and coordinates indirectly via an event listener

--- src/MapboxPlaces.js
+++ src/MapboxPlaces.js
@@ -100,6 +100,9 @@ function MapboxPlaces({
   suggestionComponent,
   geocodeQueryOptions,
   onCoordinatesUpdated,
+  eventDispatcher,
+  eventType,
+  getCurrentPlace,
 }) {
   // Wrap the Mapbox forwardGeocode service.
   const [
@@ -126,6 +129,18 @@ function MapboxPlaces({
     name: 'place_name',
     type: 'text',
   }, textInputProps, { value, onChange })
+
+  if (
+    eventDispatcher && typeof eventDispatcher.addEventListener === 'function'
+    && typeof getCurrentPlace === 'function'
+  ) {
+    eventDispatcher.addEventListener(eventType || 'change', (e) => {
+      const { placeName, coordinates } = getCurrentPlace()
+      // Simulate a Feature selection and set coordinates directly.
+      onChange(null, { newValue: placeName })
+      setCoordinates(coordinates)
+    })
+  }
 
   // Clear coordinates when the user clears the input.
   if (coordinates && value === '') {
--- src/dev.js
+++ src/dev.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom'
 import MapboxPlaces from './MapboxPlaces'
 
 const placesUi = document.getElementById('mapbox-places-ui')
+const eventDispatcher = document.getElementById('ext-submit')
+const placeNameInput = document.getElementById('ext-placename')
+const coordinatesInput = document.getElementById('ext-coordinates')
 
 // Assume the entirety of the query string is the Mapbox access token,
 // (minus the "?").
 const token = location.search.slice(1)
+
+const getCurrentPlace = () => {
+  return {
+    placeName: placeNameInput.value,
+    coordinates: coordinatesInput.value,
+  }
+}
 
 ReactDOM.render(
   <MapboxPlaces
@@ -27,6 +37,9 @@ ReactDOM.render(
       // NOTE: format is [longitude,latitude]
       proximity: [-122.4357428, 47.2365706],
     }}
+    eventDispatcher={eventDispatcher}
+    getCurrentPlace={getCurrentPlace}
+    eventType="click"
   />,
   placesUi
 )
